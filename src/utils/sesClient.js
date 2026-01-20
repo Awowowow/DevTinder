9569158a07b98d6421ae74cf8589d14abe71330c
Aarav <aaravchandel767@gmail.com>
2026-01-21 00:29:47 +0530

    please

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -5,7 +5,14 @@ const REGION = "ap-south-1";
 const sesClient = new SESClient({ region: REGION, 
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRRET_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
     },
  });
+
+ console.log("SES CONFIG:", {
+    accessKey: !!process.env.AWS_ACCESS_KEY,
+    secretKey: !!process.env.AWS_SECRET_KEY,
+    region: REGION,
+    sandbox: process.env.SES_SANDBOX_MODE,
+  });
 module.exports = { sesClient }; 
